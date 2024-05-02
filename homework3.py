b6672918c5d7b344a2c1ea78d68dbebc31c199db
SES68 <168301385+SES68@users.noreply.github.com>
2024-05-02 20:47:25 +0300

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -6,6 +6,6 @@ my_string = input("Введите первую строку стихотворе
 print (len(my_string))                         # длина строки
 print ((my_string).upper())                    # строка в верхнем регистре
 print ((my_string).lower())                    # строка в нижнем регистре
-print (my_string.replace(" ",""))  # строка без пробелов
+print (my_string.replace(" ",""))              # строка без пробелов
 print (my_string[0])                           # первый символ строки
 print (my_string[-1])                          # последний символ строки
