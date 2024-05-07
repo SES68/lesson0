74db1c5f47d1e0c4ee615aaf0a425b189a824c96
SES68 <168301385+SES68@users.noreply.github.com>
2024-05-07 09:40:40 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,8 +1,50 @@
-name=("Евгений Степанович")
-print(name)
-age=(55)
-print(age)
-age=(age-30)
-print(age)
-is_student=True
-print(is_student)
+x = 38
+
+print('дратути')
+if x < 0:
+    print('меньше нуля')
+print('дотвидания')
+
+
+a, b = 10, 5
+
+if a > b:
+    print('a > b')
+
+if a > b and a > 0:
+    print('успех')
+
+if (a > b) and (a >0 or b < 1000):
+    print('успех')
+
+if 5 < b and b < 10:
+    print('успех')
+
+
+# можно сравнивать, числа, строки, списки, вообще - все
+
+if '34' > '123':
+    print('успех')
+
+if '123' > '12':
+    print('успех')
+
+if [1,2] > [1,1]:
+    print('успех')
+
+
+
+# нельзя сравнивать разные типы/
+
+
+
+# if '6' > 5:
+#     print('успех')
+
+
+# if [5, 6] > 5:
+#     print('успех')
+
+
+#if '6' != 5:
+    print('успех')
